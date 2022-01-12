18129cb6caf4f42c21eef940f5cfc2bd9c0dc9d3
Doug Hagan <doug.hagan824@gmail.com>
2022-01-12 12:15:23 -0800

    created component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import VendingMachine from "./VendingMachine";
+import Sardines from "./WhiteClaw";
+import Chips from "./Freetos";
+import Soda from "./KitKat";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={() => <VendingMachine />} />
+          <Route path="/soda" exact render={() => <Soda />} />
+          <Route path="/chips" exact render={() => <Chips />} />
+          <Route path="/sardines" exact render={() => <Sardines />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
+
 
 export default App;
